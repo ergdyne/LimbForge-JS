21911fb700d50f86288a436869adb3a05ec9cbcd
j <github@ergdyne.com>
2019-05-16 09:35:59 -0400

    feat($server): more server boilerplate and refactoring

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-var router = express.Router()
-
-//Home page
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
-})
-
-export default router
--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,11 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import http from 'http'
 
-import indexRouter from './routes/index'
 
 const app = express()
+const server = http.createServer(app)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,6 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/', indexRouter)
+app.get('*', 
+  (req,res) => res.status(200)
+    .send({message: `Welcome to API! We have no response for ${req}.`}))
 
-export default app
+const port = 3000//parseInt(process.env.PORT,10 || 3000)
+app.set('port',port)
+
+
+server.listen(port)
